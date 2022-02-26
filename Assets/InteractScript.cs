0749b9fa892dd9bb18803870d8d2b439939f5fa2
Christopher Boyce <57403492+chris-boyce@users.noreply.github.com>
2022-02-26 22:01:50 +0000

    Fixed Wall Collisions and Then added movement the cube on all axi using raycast

--- Assets/InteractScript.cs
+++ Assets/InteractScript.cs
@@ -5,40 +5,68 @@ using UnityEngine;
 public class InteractScript : MonoBehaviour
 {
     private bool carry = false;
-    private GameObject m_player;
-    private Collider m_Collider;
+    [SerializeField] private GameObject m_player;
+    private Rigidbody m_Rigidbody;
+    private RaycastHit m_Hit;
     private bool canDrop;
+    [SerializeField] private Camera m_Camera;
 
     private void Start()
     {
-        gameObject.tag = "Interactable";
-        m_Collider = GetComponent<Collider>();
-   
+        gameObject.tag = "Interactable"; //Tags the tag so the player knows it is interactable and will run this script if interacted with 
+        if (gameObject.GetComponent<Rigidbody>() == null) //Setting Up ridgbody to freeze roation / Adds a RB if there isnt one
+        {
+            m_Rigidbody = gameObject.AddComponent<Rigidbody>();
+        }
+        else
+        {
+            m_Rigidbody = GetComponent<Rigidbody>();
+        }
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+
     }
 
-    public void objectInteract(GameObject player)
+    public void objectInteract(GameObject player , Camera camera)
     {
+        m_Camera = camera;
         m_player = player;
         carry = true;
         
     }
 
-    public void objectDrop()
+    public bool objectDrop() // Function is called from player if the player cant drop the item then carry stays true
     {
-        if (canDrop)
+        if (canDrop) // Item is dropped
         {
             carry = false;
+            return false;
+        }
+        else // Item is not dropped
+        {
+            carry = true;
+            return true;
         }
     }
     
-
-
     private void Update()
     {
-        if(carry)
+        if(carry) // Carry is true it finds the players body and goes to the postision in front of it
         {
-            Transform cubeTransform = m_player.transform.Find("Player Body");
-            transform.position = cubeTransform.position + (cubeTransform.forward * 1.5f);
+            //Transform cubeTransform = m_player.transform.Find("Player Body");
+            //transform.position = cubeTransform.position + (cubeTransform.forward * 1.5f);
+            
+            Ray ray = m_Camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            if (Physics.Raycast (ray , out m_Hit , 3))
+            {
+                Debug.DrawLine(ray.origin, ray.GetPoint(3f), Color.blue, 1);
+                transform.position = ray.GetPoint(1.75f);
+            }
+            else
+            {
+                Debug.DrawLine(ray.origin, ray.GetPoint(3f), Color.red, 1);
+                transform.position = ray.GetPoint(1.75f);
+            }
+            
             
         }
         else
@@ -46,15 +74,18 @@ public class InteractScript : MonoBehaviour
             transform.position = transform.position;
         }
     }
+
+
     public void objectRotate()
     {
         transform.Rotate(0, 90, 0);
     }
-    private void OnCollisionEnter(Collision collision)
+
+    private void OnCollisionEnter(Collision collision) //If the object is colliding with anything the player cant drop it
     {
-        canDrop = false;
+        canDrop = false; 
     }
-    private void OnCollisionExit(Collision collision)
+    private void OnCollisionExit(Collision collision) // When the object has stoppped colliding with something it can be dropped
     {
         canDrop = true;
     }
--- Assets/InteractWithObject.cs
+++ Assets/InteractWithObject.cs
@@ -8,23 +8,23 @@ public class InteractWithObject : MonoBehaviour
     private float interact;
     private float onRotate;
     public Camera playerCamera;
-    private float hit;
     public Transform player;
     private GameObject m_Player;
     private GameObject lastObject;
+    private bool isItemBeenDropped;
     public int m_playerID;
     private Controls controls;
     private bool itemPickup = false;
 
-    private void Awake()
+    private void Awake() //sets control scheme
     {
         controls = new Controls();
     }
 
-    private void OnEnable()
+    private void OnEnable() // When the player is enabled
     {
-        controls.Enable();
-        m_playerID = PlayerCountID.getPlayerID();
+        controls.Enable(); //Control enables
+        m_playerID = PlayerCountID.getPlayerID(); 
         m_Player = this.gameObject;
     }
     private void OnDisable()
@@ -32,10 +32,13 @@ public class InteractWithObject : MonoBehaviour
         controls.Disable();
     }
 
+
+    //Checking on key and reads the context / information 
     public void OnInteract(InputAction.CallbackContext context)
     {
         interact = context.ReadValue<float>();
     }
+
     public void OnRotateObject(InputAction.CallbackContext context)
     {
         onRotate = context.ReadValue<float>();
@@ -44,33 +47,33 @@ public class InteractWithObject : MonoBehaviour
 
     private void Update()
     {
-        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); //Shoots ray in the middle of players screen
         RaycastHit hit;
-        if (controls.Gameplay.Interact.triggered && interact == 1)
+        if (controls.Gameplay.Interact.triggered && interact == 1) // If the player is clicking interact / Sends onces
         { 
-            if(itemPickup)
+            if(itemPickup) // Itempickup = Has player got item in hand
             {
-                lastObject.transform.GetComponent<InteractScript>().objectDrop();
-                itemPickup = false;
+                isItemBeenDropped = lastObject.transform.GetComponent<InteractScript>().objectDrop(); // Script runs the drop and returns if the item was dropped.
+                itemPickup = isItemBeenDropped; //If item is dropped hand is classed as empty if item not dropped the hand is classed as full.
             }
-            else if (Physics.Raycast(ray, out hit))
+            else if (Physics.Raycast(ray, out hit , 3)) //If hand is empty send raycast out
             {
-                Debug.DrawRay(ray.origin, ray.direction, Color.green, 2);
-                if (hit.transform.tag == "Interactable")
+                Debug.DrawRay(ray.origin, ray.direction, Color.green, 3); // Draw ray 
+                if (hit.transform.tag == "Interactable") // If hit object that is Intecactable
                 {
-                    itemPickup = !itemPickup;
-                    lastObject = hit.transform.gameObject;
-                    hit.transform.GetComponent<InteractScript>().objectInteract(m_Player);
+                    itemPickup = true; // Item is in hand
+                    lastObject = hit.transform.gameObject; // Sets gameobject to lastobject so it can be refrenced later
+                    hit.transform.GetComponent<InteractScript>().objectInteract(m_Player, playerCamera); //Get scipt and pass in what player is doing it
 
                 }
-                else if (hit.transform.tag == "Readable")
+                else if (hit.transform.tag == "Readable") // If object is reabable send in what player and the players ID
                 {
                     hit.transform.GetComponent<ReadScript>().objectInteract(m_Player , m_playerID);
                 }
             }
 
         }
-        if(controls.Gameplay.Rotate.triggered && onRotate == 1 && itemPickup)
+        if(controls.Gameplay.Rotate.triggered && onRotate == 1 && itemPickup) // if player is pressing rotate button / does it once / and item pickup is true / call object roate in the interact script.
         {
             lastObject.GetComponent<InteractScript>().objectRotate();
         }
